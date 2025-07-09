e39f99a7223e06a0d23ac815ad63df2d10930f41
Fagner JB <para@fagnerjb.com>
2025-07-08 21:11:08 -0300

    🐛

--- dbwp/wp-content/themes/dbands/components/embed-video.php
+++ dbwp/wp-content/themes/dbands/components/embed-video.php
@@ -12,7 +12,7 @@ $oembed = str_replace('?feature=oembed', '?autoplay=1&fs=0&rel=0', $oembed);
 ?>
 <div class="embed">
    <div class="aspect-video mt-3" x-data="{show: false}">
-      <a class="relative" href="https://www.youtube.com/watch?v=<?php echo $video_ID; ?> ?>" target="_blank" x-on:click.prevent="show=!show" x-show="!show"><img src="https://i.ytimg.com/vi/<?php echo $video_ID; ?>/maxresdefault.jpg"><i class="absolute top-1/2 left-1/2 -translate-1/2 text-5xl text-white text-shadow-sm fa-brands fa-youtube"></i></a><template x-if="show"><?php echo $oembed; ?></template>
+      <a class="relative" href="https://www.youtube.com/watch?v=<?php echo $video_ID; ?>" target="_blank" x-on:click.prevent="show=!show" x-show="!show"><img src="https://i.ytimg.com/vi/<?php echo $video_ID; ?>/maxresdefault.jpg"><i class="absolute top-1/2 left-1/2 -translate-1/2 text-5xl text-white text-shadow-sm fa-brands fa-youtube"></i></a><template x-if="show"><?php echo $oembed; ?></template>
    </div>
    <div class="flex bg-zinc-800 text-zinc-200">
       <a class="flex gap-2 py-1 md:py-3 px-2 md:px-5 items-center hover:bg-neutral-700 focus-visible:bg-neutral-700"
